b03076fb69dae513ab714db41bccce288689d7a7
vinayakbhosle <vinayak7121@gmail.com>
2021-05-24 23:06:46 +0530

    .

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Student_reg from './component/Student_reg'
+
+const App=()=>{
+    return(
+        <Student_reg />
+    );
+
+}
+
+export default App;
---
+++ src/component/Student_reg.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Student_reg =() =>{
+    return(
+        <div className="ui container center aligned">
+                <h1>Student Registration</h1>
+                <br/><br/>
+        <form class="ui form">
+            <div class="field">
+                <label>First Name</label>
+                <input type="text" name="firstname" id="firstname" placeholder="First Name" />
+            </div>
+            <div class="field">
+                <label>First Name</label>
+                <input type="text" name="firstname" id="firstname" placeholder="First Name" />
+            </div>
+            <div class="field">
+                <label>First Name</label>
+                <input type="text" name="firstname" id="firstname" placeholder="First Name" />
+            </div>
+        </form>
+        </div>
+        
+    )
+}
+export default Student_reg;
